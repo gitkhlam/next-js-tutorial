e34b5b68d11654028835f06753f4462c61e79dcf
gitkhlam <illiasergukr@gmail.com>
2025-04-02 17:57:04 +0300

    add env

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,6 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// Этот файл utils.ts содержит утилитарную функцию cn, 
+// которая помогает работать с классами CSS в Tailwind CSS.
